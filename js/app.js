53d838139759697929472d4cb4c08bbc172336b3
Dylan Brown <dylandoesprogramming@gmail.com>
2017-07-26 12:25:57 -0500

    All dependencies loaded - framework setup

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('movieSite', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+    });
---
+++ js/mainSrvc.js
@@ -0,0 +1,3 @@
+app.service("mainSrvc", function($http) {
+
+});
